71f0126e7a263cdd58e561500b92db20833e7ea5
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-06 13:41:27 +0100

    Add: solver.rb file created with a factorial function to make the test pass

---
+++ solver.rb
@@ -0,0 +1,7 @@
+class Solver 
+  def factorial(num)
+    return 1 if [0, 1].include?(num)
+    raise StandardError, 'You cannot calculate factorial of a negative number' if num.negative?
+    num * factorial(num - 1)
+  end 
+end
