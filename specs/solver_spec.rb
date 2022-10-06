81aaf61c9ec8987fd740d0e6871cd8fe74bc3d9a
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-06 14:15:59 +0100

    Update: solver_spec.rb file

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,23 +1,31 @@
 require 'rspec'
 require './solver'
 
-describe 'Factorial' do
+RSpec.describe 'Solver class' do
   before :all do
     @solver = Solver.new
   end
-  it 'computes the factorial of 0' do
-    expect(@solver.factorial(0)).to eq 1
+
+  describe 'Solver factorial method' do 
+    it 'computes the factorial of 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+    it 'computes the factorial of 1' do
+      expect(@solver.factorial(1)).to eq 1
+    end
+    it 'computes the factorial of 3' do
+      expect(@solver.factorial(3)).to eq 6
+    end
+    it 'raises exception for negative integer' do
+      expect { @solver.factorial(-1) }.to raise_error('You cannot calculate factorial of a negative number')
+    end
   end
-  it 'computes the factorial of 1' do
-    expect(@solver.factorial(1)).to eq 1
+
+  describe 'Solver reverse string method' do 
+    it 'takes hello and returns olleh' do
+      expect(@solver.reverse("hello")).to eq("olleh")
+    end
   end
-  it 'computes the factorial of 3' do
-    expect(@solver.factorial(3)).to eq 6
-  end
-  it 'raises exception for negative integer' do
-    expect { @solver.factorial(-1) }.to raise_error('You cannot calculate factorial of a negative number')
-  end
-  it 'takes hello and returns olleh' do
-    expect(@solver.reverse(hello)).to eq("olleh")
-  end
+
 end
+  
