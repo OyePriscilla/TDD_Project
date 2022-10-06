51be15f26159e01396d535356078225d7075b0d8
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-06 14:25:51 +0100

    Update: solver.rb file by adding fizzbuzz method to make the failing test pass

--- solver.rb
+++ solver.rb
@@ -8,5 +8,17 @@ class Solver
   def reverse(str)
     str.reverse
   end
-  
+
+  def fizzbuzz(num)
+    if num % 3 == 0 
+      'fizz'
+    elsif num % 5 == 0
+      'buzz'
+    elsif num % 15 == 0
+      'fizzbuzz'
+    else
+      num
+    end
+  end
+
 end
