de961a226d29b04c2bff29de9a0993ea0b21e61d
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-06 13:54:21 +0100

    Write method to reverse string

--- solver.rb
+++ solver.rb
@@ -1,7 +1,11 @@
-class Solver 
+class Solver
   def factorial(num)
     return 1 if [0, 1].include?(num)
     raise StandardError, 'You cannot calculate factorial of a negative number' if num.negative?
     num * factorial(num - 1)
-  end 
+  end
+
+  def reverse(str)
+    str.reverse
+  end
 end
